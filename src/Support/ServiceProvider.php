e51ebd3db8bb2b16e83a8fd4fc79289ccd33b593
Purvesh <ind.purvesh@gmail.com>
2016-10-23 10:45:54 +1300

    support theme rewrite

--- src/Support/ServiceProvider.php
+++ src/Support/ServiceProvider.php
@@ -5,9 +5,10 @@ use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
 
 class ServiceProvider extends LaravelServiceProvider {
  
-    public function register() {
-        
-    }
-    
-  
+   public function boot() {
+       
+   }
+   public function register() {
+       
+   }
 }
--- src/Theme/ThemeManager.php
+++ src/Theme/ThemeManager.php
@@ -1,43 +1,25 @@
 <?php
+
 namespace Mage2\Framework\Theme;
 
 use Illuminate\Support\Collection;
 use Symfony\Component\Finder\Iterator\RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
 
-class ThemeManager
-{
+class ThemeManager {
 
     public $themeList;
     public $themeLoaded = false;
 
-    public function __construct()
-    {
+    public function __construct() {
         $this->themeList = Collection::make([]);
     }
 
-    public function all()
-    {
-        if($this->themeLoaded === false) {
+    public function all() {
+        if ($this->themeLoaded === false) {
             $this->loadThemes();
         }
         return $this->themeList;
-    }
-
-    public function get($name)
-    {
-        if($this->themeLoaded === false) {
-            $this->loadThemes();
-        }
-
-        $theme = $this->themeList->pull($name,null);
-
-        if(null === $theme) {
-            throw new \Exception('Sorry Could not find theme');
-        }
-
-
-        return $theme;
     }
 
     protected function loadThemes() {
@@ -46,7 +28,7 @@ class ThemeManager
 
 
         $iterator = new RecursiveIteratorIterator(
-            new RecursiveDirectoryIterator($themePath, RecursiveDirectoryIterator::FOLLOW_SYMLINKS)
+                new RecursiveDirectoryIterator($themePath, RecursiveDirectoryIterator::FOLLOW_SYMLINKS)
         );
 
         $iterator->setMaxDepth(2);
@@ -56,7 +38,7 @@ class ThemeManager
 
             if (($iterator->getDepth() > 1) && $iterator->isFile() && ($iterator->getFilename() == "ThemeInfo.php")) {
 
-                $filePath = $iterator->getPathname() ;
+                $filePath = $iterator->getPathname();
                 $themeInfo = include_once $filePath;
                 $this->themeList->put($themeInfo['name'], $themeInfo);
             }
@@ -67,4 +49,34 @@ class ThemeManager
 
         return $this;
     }
+
+    public function put($identifier, $themeInfo) {
+        $this->themeList->put($identifier, $themeInfo);
+        return $this;
+    }
+
+    public function get($identifier) {
+        return $this->themeList->pull($identifier);
+    }
+    
+    public function getByPath($path) {
+        
+       
+        foreach ($this->themeList as $theme  =>  $themeInfo) {
+            $path1 = $this->pathSlashFix($path);
+            $path2 = $this->pathSlashFix($themeInfo['path']);
+            
+            if($path1 == $path2) {
+                $actualTheme = $this->themeList[$theme];
+                break;
+            }   
+        }        
+        return $actualTheme;
+    }
+    
+    public function pathSlashFix($path) {
+        return (DIRECTORY_SEPARATOR === '\\') ? str_replace('/', '\\', $path) : str_replace('\\', '/', $path);
+        
+    }
+
 }
