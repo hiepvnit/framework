ff033bbd6acbe5dd598fa64c5f2f1ac3ed2eb64f
Purvesh <ind.purvesh@gmail.com>
2016-11-11 10:54:18 +1300

    admin menu and theme facade with service provider

--- src/Foundation/Providers/ModuleServiceProvider.php
+++ src/Foundation/Providers/ModuleServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Support\ServiceProvider;
 use Mage2\Framework\Configuration\ConfigurationServiceProvider;
 use Mage2\Framework\Payment\PaymentServiceProvider;
 use Mage2\Framework\Shipping\ShippingServiceProvider;
+use Mage2\Framework\AdminMenu\AdminMenuServiceProvider;
+use Mage2\Framework\Theme\ThemeServiceProvider;
 
 class ModuleServiceProvider extends ServiceProvider
 {
@@ -16,39 +18,12 @@ class ModuleServiceProvider extends ServiceProvider
      */
     protected $defer = true;
 
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-
-    }
-
-    /**
-     * @todo not using this feature now
-     * Register Custom Extension and add Namespace into Class Loader
-     *
-     * @return void
-     */
-    public function registerModule()
-    {
-        //$mage2Module = config('module');
-        //foreach ($mage2Module as $namespace => $path) {
-        //    $loader = new ClassLoader();
-        //    $loader->addPsr4($namespace, $path);
-        //    $loader->register();
-        //    //Register ServiceProvider for Modules
-        //    $extensionProvider = str_replace('\\', '', $namespace.'ServiceProvider');
-        //    App::register($namespace.$extensionProvider);
-        //}
-    }
-
     protected $providers = [
         ConfigurationServiceProvider::class,
         PaymentServiceProvider::class,
         ShippingServiceProvider::class,
+        AdminMenuServiceProvider::class,
+        ThemeServiceProvider::class,
     ];
 
 }
--- src/System/Module.php
+++ src/System/Module.php
@@ -2,10 +2,6 @@
 
 namespace Mage2\Framework\System;
 
-use Composer\Autoload\ClassLoader;
-use Illuminate\Support\Facades\App;
-use Mage2\Framework\Theme\ThemeManager;
-use Mage2\Framework\System\View\AdminMenu;
 use Mage2\Framework\System\Middleware\Website as WebsiteMiddleware;
 use Mage2\Framework\System\View\FileViewFinder;
 use Illuminate\View\ViewServiceProvider as BaseModule;
@@ -24,15 +20,7 @@ class Module extends BaseModule {
      * @return void
      */
     public function boot() {
-        //
-        $this->registerAdminMenuFacade();
-        
-
-        //$this->registerPaymentManager();
-        //$this->_registerPaymentFacade();
-
         $this->_registerThemeFacade();
-
         $this->app['request']->server->set('HTTPS', 'off');
     }
 
@@ -48,16 +36,7 @@ class Module extends BaseModule {
 
             return new FileViewFinder($app['files'], $paths);
         });
-
-
         $this->registerMiddleware();
-        //$this->mapWebRoutes();
-        //$this->registerAdminMenu();
-        //$this->registerAdminConfiguration();
-        //$this->registerViewPath();
-        $this->registerTheme();
-
-        //$this->registerUrlGenerator();
     }
 
     /**
@@ -69,37 +48,6 @@ class Module extends BaseModule {
 
         $router = $this->app['router'];
         $router->middleware('website', WebsiteMiddleware::class);
-
-
-        //$router = $this->app['router'];
-        //$router->middleware('web', EncryptCookies::class);
-        //$router->middleware('web', VerifyCsrfToken::class);
-    }
-
-
-    public function registerModule() {
-        $mage2Module = config('module');
-        foreach ($mage2Module as $namespace => $path) {
-            $loader = new ClassLoader();
-            $loader->addPsr4($namespace, $path);
-            $loader->register();
-            //Register ServiceProvider for Modules
-            $extensionProvider = str_replace('\\', '', $namespace . 'ServiceProvider');
-            App::register($namespace . $extensionProvider);
-        }
-    }
-
-    public function registerTheme() {
-        $mage2Module = config('theme');
-        foreach ($mage2Module as $namespace => $path) {
-            $loader = new ClassLoader();
-            $loader->addPsr4($namespace, $path);
-            $loader->register();
-            //Register ServiceProvider for Modules
-            //$extensionProvider = str_replace("\\", "", $namespace. "\\" . "ThemeInfo");
-            //dd($namespace . "ThemeInfo");die;
-            App::register($namespace . 'ThemeInfo');
-        }
     }
 
     /**
@@ -113,26 +61,5 @@ class Module extends BaseModule {
         };
     }
 
-    private function _registerThemeFacade() {
-        App::bind('Theme', function () {
-            return new ThemeManager();
-        });
-    }
-
-    public function registerAdminMenuFacade() {
-
-        App::bind('AdminMenu', function () {
-            return new AdminMenu();
-        });
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides() {
-        return ['AdminConfiguration', 'AdminMenu', 'Theme', 'Payment', 'Shipping', 'Mage2\Framework\Paymnemt\PaymentManager'];
-    }
 
 }
