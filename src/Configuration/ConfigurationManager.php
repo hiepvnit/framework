1b8e61ad4d11fe8c94990a67dff1b3f79ac7bee2
Purvesh <ind.purvesh@gmail.com>
2017-02-11 11:22:01 +1300

    sort order for system ConfigurationManager

--- src/Configuration/ConfigurationManager.php
+++ src/Configuration/ConfigurationManager.php
@@ -2,17 +2,32 @@
 
 namespace Mage2\Framework\Configuration;
 
+use Illuminate\Support\Collection;
+
 class ConfigurationManager
 {
     protected $adminConfiguration;
 
-    public function registerConfiguration($adminConfiguration)
-    {
-        $this->adminConfiguration[] = $adminConfiguration;
+
+    public function __construct() {
+        $this->adminConfiguration = Collection::make([]);
     }
 
-    public function All()
+    public function registerConfiguration($adminConfiguration)
     {
+        if(!isset($adminConfiguration['sort_order'])) {
+            $adminConfiguration['sort_order'] = 100;
+        }
+        $this->adminConfiguration->push($adminConfiguration);
+    }
+
+    public function All($sorted = true)
+    {
+        if(true === $sorted) {
+            $sorted = $this->adminConfiguration->sortBy('sort_order');
+            return $sorted->values()->all();
+        }
+
         return $this->adminConfiguration;
     }
 }
