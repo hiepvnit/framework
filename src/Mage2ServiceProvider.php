4d7e19fa58f0b37e80e7eb86cf9ac3a4edd550fd
indpurvesh <ind.purvesh@gmail.com>
2017-09-10 14:20:41 +1200

    updated

--- src/Mage2ServiceProvider.php
+++ src/Mage2ServiceProvider.php
@@ -71,30 +71,4 @@ class Mage2ServiceProvider extends ServiceProvider
     {
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'mage2-framework');
     }
-
-    public function extendView() {
-
-        App::extend('view',function() {
-            $app = app();
-            // Next we need to grab the engine resolver instance that will be used by the
-            // environment. The resolver will be used by an environment to get each of
-            // the various engine implementations such as plain PHP or Blade engine.
-            $resolver = $app['view.engine.resolver'];
-
-            $finder = $app['view.finder'];
-
-            $env = new \Mage2\Framework\View($resolver, $finder, $app['events']);
-
-            // We will also set the container instance on this view environment since the
-            // view composers may be classes registered in the container, which allows
-            // for great testable, flexible composers for the application developer.
-            $env->setContainer($app);
-
-            $env->share('app', $app);
-
-            return $env;
-
-        });
-
-    }
 }
